6532ac41aa99651dd065c51afe1229127c85a269
Mahir Morshed <bdcoder89@gmail.com>
2026-09-18 23:55:51 +0600

    added custom icon and fixed playback issue

--- android/MahirTV/app/src/main/java/com/example/mahirtv/PlaybackVideoFragment.kt
+++ android/MahirTV/app/src/main/java/com/example/mahirtv/PlaybackVideoFragment.kt
@@ -8,28 +8,43 @@ import androidx.leanback.media.MediaPlayerAdapter
 import androidx.leanback.media.PlaybackTransportControlGlue
 import androidx.leanback.widget.PlaybackControlsRow
 
+import android.view.WindowManager
+
 /** Handles video playback with media controls. */
 class PlaybackVideoFragment : VideoSupportFragment() {
 
-    private lateinit var mTransportControlGlue: PlaybackTransportControlGlue<MediaPlayerAdapter>
+    private lateinit var mTransportControlGlue:
+            PlaybackTransportControlGlue<MediaPlayerAdapter>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requireActivity().window.addFlags(
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+        )
 
         val (_, title, description, _, _, videoUrl) =
             activity?.intent?.getSerializableExtra(PlaybackActivity.MOVIE) as Movie
 
         val glueHost = VideoSupportFragmentGlueHost(this@PlaybackVideoFragment)
-        val playerAdapter = MediaPlayerAdapter(activity)
-        playerAdapter.setRepeatAction(PlaybackControlsRow.RepeatAction.INDEX_NONE)
 
-        mTransportControlGlue = PlaybackTransportControlGlue(getActivity(), playerAdapter)
+        val playerAdapter = MediaPlayerAdapter(activity)
+
+        playerAdapter.setRepeatAction(
+            PlaybackControlsRow.RepeatAction.INDEX_NONE
+        )
+
+        mTransportControlGlue =
+            PlaybackTransportControlGlue(getActivity(), playerAdapter)
+
         mTransportControlGlue.host = glueHost
         mTransportControlGlue.title = title
         mTransportControlGlue.subtitle = description
-        mTransportControlGlue.playWhenPrepared()
+        mTransportControlGlue.setSeekEnabled(true)
 
         playerAdapter.setDataSource(Uri.parse(videoUrl))
+
+        mTransportControlGlue.playWhenPrepared()
     }
 
     override fun onPause() {
